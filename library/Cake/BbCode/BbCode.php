f6b94216081c1d9aae52d10dfc51b3eec987dcf9
Jonathan Wainwright <jon@waindigo.org>
2015-07-29 13:36:14 +0100

    Added blank BB code callback

---
+++ library/Cake/BbCode/BbCode.php
@@ -0,0 +1,10 @@
+<?php
+namespace Cake\BbCode;
+
+class BbCode
+{
+    public static function blank(array $tag, array $rendererStates, \XenForo_BbCode_Formatter_Base $formatter)
+    {
+        return '';
+    }
+}
