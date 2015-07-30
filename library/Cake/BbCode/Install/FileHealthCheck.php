a031a9246b5b27e963a96144c2081c0a0f783951
Jonathan Wainwright <jon@waindigo.org>
2015-07-30 20:24:59 +0100

    Remove FileHealthCheck.php from file health check

--- library/Cake/BbCode/Install/FileHealthCheck.php
+++ library/Cake/BbCode/Install/FileHealthCheck.php
@@ -10,7 +10,6 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
         	'library/Cake/BbCode/BbCode.php' => 'abff4027bb85d3bba0c6b7f082eca4df',
         	'library/Cake/BbCode/Install/Controller.php' => '9cc17c42e7e6ee05587100060c907470',
         	'library/Cake/BbCode/Install/Data.php' => '0716d959efba6a6538b30af72efec392',
-        	'library/Cake/BbCode/Install/FileHealthCheck.php' => '73f52a5722137800fc2bd8a18f842d96',
         	'library/Cake/BbCode/Proxy/XenForo/ControllerAdmin/BbCode.php' => '8eea0face221192f8f09b9b218801c00',
         	'library/Cake/BbCode/Proxy/XenForo/DataWriter/BbCode.php' => '5c5976919665aac0b026c1ab1206d25d',
         	'library/Cake/BbCode/Proxy/XenForo/Model/BbCode.php' => '8a23924045e47497c9a3f129fa84161a',
